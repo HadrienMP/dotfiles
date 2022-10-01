175515b189590a2f9a4adeaefd6e7905670dd8fb
hadrienmp <github@hadrienmp.fr>
2022-10-01 12:22:35 +0200

    add a setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo $0
+currentDir=`dirname "$0"`
+echo $currentDir
+#ln -s $currentDir/.vimrc ~/.vimrc 
+#ln -s $currentDir/tmux.conf ~/.tmux.conf
+echo "[include]" >> ~/.gitconfig
+echo "  path=${currentDir}/.gitconfig" >> ~/.gitconfig
