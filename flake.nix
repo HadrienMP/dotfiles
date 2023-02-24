b4c8a6d45fd48f72534287d4a3be6157b6a3a333
hadrienmp <github@hadrienmp.fr>
2023-02-24 13:50:55 +0100

    add setup for linux

--- flake.nix
+++ flake.nix
@@ -27,6 +27,10 @@
         pkgs = nixpkgs.legacyPackages.x86_64-darwin;
         modules = [ ./home.nix ];
       };
+      "h" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./home.nix ];
+      };
     };
   };
 }
