0d5b50e9d34479f975b2e3a0470bafda5a485e12
hadrienmp <github@hadrienmp.fr>
2023-04-04 16:47:24 +0200

    add buffer switch keys
    H for left
    L for right

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,7 +10,14 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- Disable arrow keys
 map("", "<up>", "<nop>")
 map("", "<down>", "<nop>")
 map("", "<left>", "<nop>")
 map("", "<right>", "<nop>")
+
+-- Switch buffers
+-- <S-r> means capital R (Shift + R)
+-- Bépo mapping
+map("n", "<S-l>", ":bnext<CR>")
+map("n", "<S-h>", ":bprevious<CR>")
