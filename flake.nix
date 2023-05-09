bc4517bb52ff7015ef707508c53a1a018d9e2107
hadrienmp <github@hadrienmp.fr>
2023-05-09 09:55:52 +0200

    fix: use arm64 for macos setup

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Home Manager Flake";
-  
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
@@ -9,28 +9,28 @@
     };
   };
 
-  outputs = {nixpkgs, home-manager, ...}: 
+  outputs = { nixpkgs, home-manager, ... }:
     let
       overlay-unstable = final: prev: {
         unstable = import nixpkgs {
           config.allowUnfree = true;
         };
       };
-    in {
+    in
+    {
 
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
-    defaultPackage.aarch64-darwin = home-manager.defaultPackage.x86_64-darwin;
+      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+      defaultPackage.aarch64-darwin = home-manager.defaultPackage.x86_64-darwin;
 
-    homeConfigurations = {
-      "ash" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-        modules = [ ./home.nix ];
-      };
-      "h" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [ ./home.nix ];
+      homeConfigurations = {
+        "ash" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          modules = [ ./home.nix ];
+        };
+        "h" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home.nix ];
+        };
       };
     };
-  };
 }
