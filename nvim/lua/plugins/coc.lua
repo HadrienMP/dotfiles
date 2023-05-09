29ca67eedb39b174aba55c97d3593c90f008098e
hadrienmp <github@hadrienmp.fr>
2023-05-09 10:17:06 +0200

    feat: add todo comments plugin

--- nvim/lua/plugins/coc.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "neoclide/coc.nvim",
-    enabled = false,
-    branch = "release",
-    keys = { { "<leader>cr", "<Plug>(coc-codeaction-refactor)", desc = "Refactor" } },
-  },
-}
---
+++ nvim/lua/plugins/todos.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "folke/todo-comments.nvim",
+        enabled = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+}
