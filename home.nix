422c6e11dbbd1efedfd21cde55e4ab4b6c25f5f2
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-06-08 15:54:31 +0200

    refactor(tmux): improve config

--- home.nix
+++ home.nix
@@ -92,46 +92,38 @@
   programs.tmux = {
     enable = true;
     plugins = with pkgs.tmuxPlugins; [
-      resurrect
-      continuum
-      power-theme
-      vim-tmux-navigator
+      {
+        plugin = power-theme;
+        extraConfig = "set -g @tmux_power_theme 'violet'";
+      }
+      {
+        plugin = vim-tmux-navigator;
+        extraConfig = ''
+          # Smart pane switching with awareness of Vim splits.
+          # See: https://github.com/christoomey/vim-tmux-navigator
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+              | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$'"
+          bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+          bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+          bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+          bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+          tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+          if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+          if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+          bind-key -T copy-mode-vi 'C-h' select-pane -L
+          bind-key -T copy-mode-vi 'C-j' select-pane -D
+          bind-key -T copy-mode-vi 'C-k' select-pane -U
+          bind-key -T copy-mode-vi 'C-l' select-pane -R
+          bind-key -T copy-mode-vi 'C-\' select-pane -l
+        '';
+      }
     ];
-    extraConfig = ''
-      # Mouse works as expected
-      set-option -g mouse on
-
-      set -g @continuum-restore 'on'
-      set -g @continuum-boot 'on'
-      set -g status-right 'Continuum status: #{continuum_status}'
-
-      set-option -sg escape-time 10
-      set-option -g default-terminal "screen-256color"
-      set-option -sa terminal-overrides ',XXX:RGB'
-
-      set -g @tmux_power_theme 'violet'
-
-      # Jump from tmux to vim and backw
-      # Smart pane switching with awareness of Vim splits.
-      # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-          | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$'"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-
-      bind-key -T copy-mode-vi 'C-h' select-pane -L
-      bind-key -T copy-mode-vi 'C-j' select-pane -D
-      bind-key -T copy-mode-vi 'C-k' select-pane -U
-      bind-key -T copy-mode-vi 'C-l' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l    
-    '';
+    keyMode = "vi";
+    mouse = true;
+    sensibleOnTop = false;
   };
 
   # ---------------------------------------
@@ -140,7 +132,6 @@
   programs.tmate = {
     enable = true;
     extraConfig = ''
-      if-shell "test -f ~/.tmux-status.conf" "source ~/.tmux-status.conf"
     '';
   };
 
