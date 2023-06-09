4f14282ade7d27584850ea14b882ad442d2623f1
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-06-09 17:48:47 +0200

    chore: delete emoji cmp use only gitmoji

--- nvim/lua/plugins/cmp-emoji.lua
+++ nvim/lua/plugins/cmp-emoji.lua
@@ -1,12 +1,11 @@
 return {
     {
         "hrsh7th/nvim-cmp",
-        dependencies = { "hrsh7th/cmp-emoji", "megalithic/cmp-gitmoji" },
+        dependencies = { "megalithic/cmp-gitmoji" },
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
             local cmp = require("cmp")
-            opts.sources =
-                cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" }, { name = "gitmoji" } }))
+            opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "gitmoji" } }))
         end,
     },
 }
