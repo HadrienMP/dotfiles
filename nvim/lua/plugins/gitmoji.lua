5621b745f384cb629e959751e6e70e80a1be56d4
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-06-19 08:57:53 +0200

    chore: disable gitmoji (broken rendering in vim)

---
+++ nvim/lua/plugins/gitmoji.lua
@@ -0,0 +1,12 @@
+return {
+    -- {
+    --     "hrsh7th/nvim-cmp",
+    --     enable = false,
+    --     dependencies = { "megalithic/cmp-gitmoji" },
+    --     ---@param opts cmp.ConfigSchema
+    --     opts = function(_, opts)
+    --         local cmp = require("cmp")
+    --         opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "gitmoji" } }))
+    --     end,
+    -- },
+}
