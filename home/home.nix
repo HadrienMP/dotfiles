0e4d57756d8ac5d348b10f2028c3236791fe9a15
hadrienmp <github@hadrienmp.fr>
2025-09-07 15:09:54 +0200

    feat: improve tmux catppuccin theme

--- home/home.nix
+++ home/home.nix
@@ -107,10 +107,18 @@
   programs.tmux = {
     enable = true;
     plugins = with pkgs.tmuxPlugins; [
+      { plugin = cpu; }
       {
         plugin = catppuccin;
         extraConfig = ''
-          set -g @catppuccin_window_middle_separator "\ue0b4 "
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
         '';
       }
       {
