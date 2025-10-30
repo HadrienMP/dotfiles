ac7363c92e7b0c6288fddaa0c1ad7e3ab39067ce
HadrienMP <github@hadrienmp.fr>
2025-10-30 23:29:16 +0100

    chore(git): new format

--- home/git.nix
+++ home/git.nix
@@ -3,26 +3,24 @@
 {
   programs.git = {
     enable = true;
-    userName = "hadrienmp";
-    userEmail = "github@hadrienmp.fr";
-    aliases = {
-      co = "checkout";
-      s = "status";
-      ci = "commit";
-      cim = "commit -m";
-      ciam = "commit -am";
-      a = "add";
-      p = "pull";
-      push-please = "push --force-with-lease";
-      cp = "cherry-pick";
-      ls = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
-      ll = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
-      hide = "update-index --assume-unchanged";
-      unhide = "update-index --no-assume-unchanged";
-      show-hidden = "!git ls-files -v | grep '^h' | cut -c3";
-      housekeeping = "!git branch --merged | grep -v main | xargs git branch -d";
-    };
-    extraConfig = {
+    settings = {
+      alias = {
+        co = "checkout";
+        s = "status";
+        ci = "commit";
+        cim = "commit -m";
+        ciam = "commit -am";
+        a = "add";
+        p = "pull";
+        push-please = "push --force-with-lease";
+        cp = "cherry-pick";
+        ls = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
+        ll = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
+        hide = "update-index --assume-unchanged";
+        unhide = "update-index --no-assume-unchanged";
+        show-hidden = "!git ls-files -v | grep '^h' | cut -c3";
+        housekeeping = "!git branch --merged | grep -v main | xargs git branch -d";
+      };
       core = {
         autocrlf = "input";
         editor = "vim";
@@ -35,6 +33,10 @@
       };
       push = {
         default = "current";
+      };
+      user = {
+        name = "hadrienmp";
+        email = "github@hadrienmp.fr";
       };
     };
   };
