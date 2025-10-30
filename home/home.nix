55639095381f032c0a78a674c354eeb76f83868f
HadrienMP <github@hadrienmp.fr>
2025-10-30 23:29:50 +0100

    chore(ssh): new format

--- home/home.nix
+++ home/home.nix
@@ -89,10 +89,12 @@
   # ---------------------------------------
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-        IdentityAgent ~/.1password/agent.sock
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      extraOptions = {
+        IdentityAgent = "~/.1password/agent.sock";
+      };
+    };
   };
 
 
