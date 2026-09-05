b03a504c93ced622f5479ea340cb34cba83f011a
hadrienmp <github@hadrienmp.fr>
2026-09-05 17:00:32 +0200

    chore: handle ssh api changes

--- home/system-specific/aarch64-darwin-home.nix
+++ home/system-specific/aarch64-darwin-home.nix
@@ -2,5 +2,7 @@
   home.username = "ash";
   home.homeDirectory = "/Users/ash";
 
-  programs.ssh.matchBlocks."*".extraOptions.IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+  programs.ssh.settings."*" = {
+    IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+  };
 }
